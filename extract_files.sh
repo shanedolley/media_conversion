f882df0c3497df1e99bbabd7f437d73ba76b0e91
Shane Dolley <shane@dolley.au>
2024-01-27 15:17:07 +1000

    Added category column to output file

--- extract_files.sh
+++ extract_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-find '/mnt/plexmedia/Movies' -type f -size +5M -printf '%f|%s\n' > ./files.csv
-find '/mnt/plexmedia/TV' -type f -size +5M -printf '%f|%s\n' >> ./files.csv
-find '/mnt/plexmedia/Comedy' -type f -size +5M -printf '%f|%s\n' >> ./files.csv
-find '/mnt/plexmedia/Documentaries' -type f -size +5M -printf '%f|%s\n' >> ./files.csv
-find '/mnt/plexmedia/Theatre' -type f -size +5M -printf '%f|%s\n' >> ./files.csv
+find '/mnt/plexmedia/Movies' -type f -size +5M -printf 'Movies|%f|%s\n' > ./files.csv
+find '/mnt/plexmedia/TV' -type f -size +5M -printf 'TV|%f|%s\n' >> ./files.csv
+find '/mnt/plexmedia/Comedy' -type f -size +5M -printf 'Comedy|%f|%s\n' >> ./files.csv
+find '/mnt/plexmedia/Documentaries' -type f -size +5M -printf 'Documentaries|%f|%s\n' >> ./files.csv
+find '/mnt/plexmedia/Theatre' -type f -size +5M -printf 'Theatre|%f|%s\n' >> ./files.csv
