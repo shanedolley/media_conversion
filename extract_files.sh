5225d3b122aebccb1706e1176fad99cd8dbc85de
Shane Dolley <shane@dolley.au>
2024-01-27 15:12:50 +1000

    Added script to create filenames

---
+++ extract_files.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# find '/mnt/plexmedia/Movies' -type f -size +5M -printf '%f|%s\n' > ./files.csv
+# find '/mnt/plexmedia/TV' -type f -size +5M -printf '%f|%s\n' >> ./files.csv
+# find '/mnt/plexmedia/Comedy' -type f -size +5M -printf '%f|%s\n' >> ./files.csv
+# find '/mnt/plexmedia/Documentaries' -type f -size +5M -printf '%f|%s\n' >> ./files.csv
+find '/mnt/plexmedia/Theatre' -type f -size +5M -printf '%f|%s\n' >> ./files.csv
