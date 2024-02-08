775e7e7e84a6641b0287512c1071a830346ea8b8
Shane Dolley <shane@dolley.au>
2024-02-08 18:59:18 +1000

    Test update

--- test.sh
+++ test.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+foldername="/mnt/plexmedia/Movies/The Deer Hunter (1978) {tmdb-11778}"
+filename=The Deer Hunter (1978) [Remux-1080p] {tmdb-11778}.mp4
+convFileName="$foldername/$filename"
 
+ffmpeg -i "$convFileName" -c:v hevc_nvenc -c:a copy -stats -loglevel quiet -strict -2 -y "/conversions/$filename"
